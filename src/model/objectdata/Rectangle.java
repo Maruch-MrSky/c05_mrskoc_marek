dcc173f682b2b4265fea24de5608dbcceb6a4f8c
Mrsky Mařuch <mrskocmar@seznam.cz>
2025-11-15 14:39:20 +0100

    uprava rectangle na horizontální základnu (jako v malování)

--- src/model/objectdata/Rectangle.java
+++ src/model/objectdata/Rectangle.java
@@ -1,40 +1,38 @@
+// src/model/objectdata/Rectangle.java
 package model.objectdata;
 
 public class Rectangle extends Polygon {
 
+    private static final double EPS = 1e-9;
+
     public Rectangle(Point2D point1, Point2D point2, Point2D heightPoint) {
-        // rectangle dostane dva body základny a jeden bod určující výšku
-        // z těchto bodů počítáme třetí a čtvrtý bod
-        // heightPoint neukládáme
         super();
+
         double ax = point1.getX();
         double ay = point1.getY();
         double bx = point2.getX();
         double by = point2.getY();
-        double hx = heightPoint.getX();
-        double hy = heightPoint.getY();
 
-        double dx = bx - ax;
-        double dy = by - ay;
-        double baseLenght = Math.sqrt(dx * dx + dy * dy);
-        if (baseLenght == 0.0) {
-            throw new IllegalArgumentException("");
+        // základna musí být horizontální
+        Line base = new Line(point1, point2);
+        if (!base.isHorizontal()) {
+            throw new IllegalArgumentException("Základna musí být horizontální.");
+        }
+        // nenulová délka základny
+        double baseLength = Math.abs(bx - ax);
+        if (baseLength == 0.0) {
+            throw new IllegalArgumentException("Základna musí být nenulová.");
+        }
+        // nenulová výška
+        double height = heightPoint.getY() - ay;
+        if (height == 0.0) {
+            throw new IllegalArgumentException("Výška musí být nenulová.");
         }
 
-        // jednotkový normál k základu
-        double nx = -dy / baseLenght;
-        double ny = dx / baseLenght;
-
-        // výška
-        double ahx = hx - ax;
-        double ahy = hy - ay;
-        double height = ahx * nx + ahy * ny;
-
-        // vrcholy obdélníku
         Point2D A = point1;
         Point2D B = point2;
-        Point2D C = new Point2D(bx + nx * height, by + ny * height);
-        Point2D D = new Point2D(ax + nx * height, ay + ny * height);
+        Point2D C = new Point2D(bx, by + height);
+        Point2D D = new Point2D(ax, ay + height);
 
         addItem(A);
         addItem(B);
@@ -42,4 +40,3 @@ public class Rectangle extends Polygon {
         addItem(D);
     }
 }
-
